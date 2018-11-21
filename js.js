b52eebe4d43e1bcd1ff41d1e3fe1e416795a8ef1
Arthur Gonçalves <arthurgoncalves@infojr.com.br>
2018-11-21 20:58:15 -0300

    Add files via upload

--- js.js
+++ js.js
@@ -1,17 +1,20 @@
-var base = $.get("movies.html");
-$("h1").click(function(){
-	$("h1").css("color", "yellow");
-	});
+var base;
+$.ajax("https://leandrojsa.github.io/movies.html", {
+	async: false,
+	success: function(data){
+		base = data;
+	}
+});
+var filmes = base.children[34].getElementsByClassName(".movie");
+console.log($(filmes).children(".movie"));
 $("button").click(function() {
 	console.log("mensagem");
+
 	var ano = $('#Ano').val();
 	var atores = $('#Atores').val();
 	var categoria = $('#Categoria option:selected').text();
 	var outras = $('#outras').val();
-	$(base).children(".movie").each(function(){
-		if ($(this).children(".categories:contains('"+categoria+"'").length > 0) {
-			console.log($(this).context.outerHTML);
-			console.log("mensagem");
-		};
-	});
+	for (var i = 3; i <= 4; i++) {
+		console.log($(base).find("#"+i));
+	}
 });
